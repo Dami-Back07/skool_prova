fba4b915c38129f07d744f80a1f5cafc8b10251a
dami <gitDami07@gmail.com>
2024-11-01 11:47:06 +0100

    aggiunta di 1 una view su /users , per la visualizzazione del modello devo serializzarlo

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+
 from . import views
 
 urlpatterns = [
     path("",views.index , name="index"),
+    path("viewusers" , views.view_user , name="viewusers"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import  User
 # Create your views here.
 def index(request):
     return HttpResponse("hello world")
+
+
+
+def view_user(request):
+    #devo serializzare il modello per stamparlo
+    return HttpResponse("serializza il modello")
