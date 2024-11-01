e2c7e21b55db61d991cb912c220bfd4168f7da93
lubuntu <gitdami07@gmail.com>
2024-11-01 16:23:01 +0100

    commit 3 per railway ancora non ho fatto la serializzazione per i modelli

--- skool_prova/settings.py
+++ skool_prova/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ehf*fdbtresb)+xdsjza^ntkxnrfx%!+rj0wg5zo!^k__a6c&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
